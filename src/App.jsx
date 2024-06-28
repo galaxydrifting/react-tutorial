a7e9916e72bfc943822e2da8d81227711d626992
bruce.lu <bruce.lu@bridgewell.com>
2024-06-28 15:19:12 +0000

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              i1mage={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
