081fff5743cf92196ca3afab58eff0fd5f4cdc43
bruce.lu <bruce.lu@bridgewell.com>
2024-07-01 12:23:53 +0000

    chore: restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header';
-import CoreConcept from './components/CoreConcept';
-
 function App() {
   return (
     <div>
-      <Header />
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept
-              i1mage={CORE_CONCEPTS[0].image}
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
+        <h2>Time to get started!</h2>
       </main>
     </div>
   );
