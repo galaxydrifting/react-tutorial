22a2882612f97b5cf3b7965d6d66ca9bda66f3d1
bruce.lu <bruce.lu@bridgewell.com>
2024-07-03 15:18:21 +0000

    lesson 52

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton/TabButton';
 
 function App() {
-  function handleSelect() {
-    console.log('Tab selected');
+  function handleSelect(selectedButton) {
+    // selectedButton => 'Components', 'JSX', 'Props', or 'State'
+    console.log(selectedButton);
   }
   return (
     <div>
@@ -23,10 +24,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
       </main>
